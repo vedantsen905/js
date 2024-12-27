c6455e51640b2d55d5e68a581c603d58851a75ec
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-27 19:23:35 +0530

    new files

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+// let score = "33abc"; 
+
+// console.log(typeof score);
+
+// let newscore = Number(score)
+
+// console.log(typeof newscore);
+// console.log(newscore);
+
+
+// // 33 => NUMBER
+// // "33ABC" => Nan
+// // true => 1 , false => 0 
+// // "huetseh" => true, "" => false
+
+// let somenum = 43
+
+// let num = String(somenum)
+// console.log(num);
+// console.log(typeof num);
+
+
+let value = 3 
+let negvalue = -value
+
+console.log(negvalue);
+
+
+console.log("1" + 2);
+console.log(1 + 2);
+console.log(1 + "2");
+
+console.log(2 + 2 + "3");
+
+let gamecounter = 222
+gamecounter++
+
+console.log(gamecounter);
+
+
+
