8dd4d3f9760cb65a3ecf85bb87ae675f53b85cc0
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-28 01:13:30 +0530

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.getDate());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.getMonth());
+// console.log(typeof myDate);
+
+// let created = new Date("12-01-2024")
+// console.log(created.toLocaleString());
+
+
+console.log(Math.floor(Date.now()/1000))
+
+console.log(myDate.getUTCDay());
