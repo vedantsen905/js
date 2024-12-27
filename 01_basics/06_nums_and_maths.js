6e155a443988885c3f2f0453e4260e873c5d0927
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-28 00:55:09 +0530

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+
+// //******************************number*********************************************
+
+// const score = 40 
+// console.log(score);
+
+// const balance = new Number(400)
+
+// console.log(balance);
+
+// console.log(balance.toString(). length);
+// console.log(balance.toFixed(2));
+
+// const num = 12.39097
+
+// console.log(num.toPrecision(3));
+
+// console.log();
+
+
+
+// ************************************maths*******************************************
+
+console.log(Math);
+console.log(Math.abs(-4454));
+console.log(Math.ceil(4.2));
+console.log(Math.round(3.4));
+console.log(Math.min(3,1,2,3,4,5,7,-6));
+
+console.log(Math.random());
+console.log(Math.random()*10 );
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+
+
