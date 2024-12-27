d01729eeecd8164a32d280fee5fbeee1ac00665c
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-27 18:39:56 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 12
+let accountemail = "vedantsen"
+var password = "12345"
+account = "jaipur"
+
+
+console.log(accountID);
+console.table([accountID, accountemail, password, account ])
