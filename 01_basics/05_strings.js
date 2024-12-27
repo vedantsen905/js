32ab005ffd2cf6ea6482c79977fa2f36a8911bfb
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-28 00:27:19 +0530

    String methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+// string interpolation 
+// with ${variable name }
+let name = String("Vedant")
+let age = 34
+console.log(` Hello my name is ${name} and age is ${age}`);
+
+
+const gamename = new String('Vedant')
+
+console.log(gamename[2]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+// console.log(gamename.replace(t));
+
+const newString = "    Vedant. "
+console.log(newString);
+console.log(newString.trim);
+
+console.log(newString.replace('da' , 'me'));
+
+console.log(newString.includes('sundar'));
+
+
+// convertion of string into array 
+// split method
+// charAt() method 
+console.log(gamename.charAt(2));
+
+// charcodeAt method 
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.charCodeAt(4));
+
+// replace method 
+
+console.log(sentence.replace("The" , "Vedant"));
+
+
