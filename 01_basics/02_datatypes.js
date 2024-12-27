42bee64058ef58795cf67da1caaf37b8cb167bbd
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2024-12-27 18:37:25 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+
+console.log(5+5); 
+console.log("Hello world ")
+
+
+let name = "Vedant"
+let age = 34
+let isLoggedin = false
+
+// number => 2 to power 53 
+/// bigint
+// String => ""
+// boolean => True/false
+// Null => stand alone value
+// undefined => 
+// Symbol => unique
+
+
+// unprimitive datatype only a categoriaztion of datatype
+//type of 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
